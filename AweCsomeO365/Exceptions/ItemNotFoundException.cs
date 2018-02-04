bf10b268579d3f53b4ab4eee0074a39f87425926
Ole <ole@nocarrier.de>
2018-02-04 16:06:48 +0100

    Added Attributes and Exceptions

---
+++ AweCsomeO365/Exceptions/ItemNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AweCsomeO365.Exceptions
+{
+    public class ItemNotFoundException : Exception
+    {
+    }
+}
