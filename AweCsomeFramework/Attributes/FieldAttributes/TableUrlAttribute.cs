5ec2cd4be9110b7c5f64d2f58f7057c32dbd4da9
Ole <ole@nocarrier.de>
2019-03-24 23:03:58 +0100

    UrlAttribute-TableUrlAttribute

--- AweCsomeFramework/Attributes/FieldAttributes/TableUrlAttribute.cs
+++ AweCsomeFramework/Attributes/FieldAttributes/TableUrlAttribute.cs
@@ -3,7 +3,7 @@ using AweCsome.Enumerations;
 
 namespace AweCsome.Attributes.FieldAttributes
 {
-    public class UrlAttribute : Attribute
+    public class TableUrlAttribute : Attribute
     {
         public UrlFieldFormatType UrlFieldFormatType { get; set; } = UrlFieldFormatType.Hyperlink;
         public const string AssociatedFieldType = "URL";
--- AweCsomeFramework/AweCsomeField.cs
+++ AweCsomeFramework/AweCsomeField.cs
@@ -277,7 +277,7 @@ namespace AweCsome
         private void GetFieldCreationDetailsUrl(PropertyInfo property, out string fieldAttributes)
         {
             fieldAttributes = null;
-            var urlAttribute = property.GetCustomAttribute<UrlAttribute>();
+            var urlAttribute = property.GetCustomAttribute<TableUrlAttribute>();
             if (urlAttribute != null)
             {
                 fieldAttributes = $"Format='{urlAttribute.UrlFieldFormatType}'";
