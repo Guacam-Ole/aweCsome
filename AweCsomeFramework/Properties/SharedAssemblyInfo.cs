48119f101960d466d2dd69c9056c883134b93ee3
Ole <ole@nocarrier.de>
2019-07-31 11:17:22 +0200

    V++

--- AweCsomeFramework/Properties/SharedAssemblyInfo.cs
+++ AweCsomeFramework/Properties/SharedAssemblyInfo.cs
@@ -7,4 +7,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © 2019 Ole Albers")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("0.10.1")]
+[assembly: AssemblyVersion("0.10.2")]
