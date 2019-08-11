34ca6bd02dbce03bb8066f4e2eb916782beacc74
Ole <ole@nocarrier.de>
2019-08-11 13:47:57 +0200

    Added Classes for User and Group

---
+++ AweCsomeFramework/Entities/AweCsomeGroup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AweCsome.Entities
+{
+    public class AweCsomeGroup
+    {
+        public int Id { get; set; }
+        public bool AllowMembersEditMembership { get; set; }
+        public bool AllowRequestToJoinLeave { get; set; }
+        public bool AutoAcceptRequestToJoinLeave { get; set; }
+        public bool CanCurrentUserEditMembership { get; set; }
+        public bool CanCurrentUserManageGroup { get; set; }
+        public bool CanCurrentUserViewMembership { get; set; }
+        public string Description { get; set; }
+        public bool IsHiddenInUI { get; set; }
+        public string LoginName { get; set; }
+        public bool OnlyAllowMembersViewMembership { get; set; }
+        public string OwnerTitle { get; set; }
+        public string RequestToJoinLeaveEmailSetting { get; set; }
+        public string Title { get; set; }
+        public List<AweCsomeUser> Users { get; set; }
+
+
+
+    }
+}
---
+++ AweCsomeFramework/Entities/AweCsomeUser.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace AweCsome.Entities
+{
+    public class AweCsomeUser
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public bool IsEmailAuthenticationGuestUser { get; set; }
+        public bool IsHiddenInUI { get; set; }
+        public bool IsShareByEmailGuestUser { get; set; }
+        public bool IsSiteAdmin { get; set; }
+        public string LoginName { get; set; }
+        public string Title { get; set; }
+        public List<AweCsomeGroup> Groups { get; set; }
+    }
+}
