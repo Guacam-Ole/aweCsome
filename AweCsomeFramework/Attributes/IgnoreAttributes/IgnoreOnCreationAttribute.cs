f3b4cc351dae5e453a786eecc84a90b2b9943dba
Ole <ole@nocarrier.de>
2019-09-05 12:39:41 +0200

    Added Option to IgnoreAttribute

--- AweCsomeFramework/Attributes/IgnoreAttributes/IgnoreOnCreationAttribute.cs
+++ AweCsomeFramework/Attributes/IgnoreAttributes/IgnoreOnCreationAttribute.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AweCsome.Attributes.IgnoreAttributes
 {
-    public class IgnoreOnCreationAttribute:Attribute
+    public class IgnoreOnCreationAttribute : Attribute
     {
+
         public bool IgnoreOnCreation { get; set; } = true;
     }
 }
