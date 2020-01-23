b839b3f0c5373d130dc2bb2c3e598fa486a434c5
Ole <ole@nocarrier.de>
2020-01-23 15:39:44 +0100

    Virus Statusfield

--- AweCsomeFramework/Entities/AweCsomeFile.cs
+++ AweCsomeFramework/Entities/AweCsomeFile.cs
@@ -11,6 +11,7 @@ namespace AweCsome.Entities
 
         public enum CheckoutTypes { None, Online, Offline }
         public enum FileLevels { Checkout, Draft, Published }
+        public enum VirusStatusValues {  Clean=0, Reported=1, CanBeRemoved=2, Removed=3, CantClean=4, DeleteRequested=5, Timeout=6};
 
         public long Length { get; set; }
         public string Version { get; set; }
@@ -22,5 +23,7 @@ namespace AweCsome.Entities
         public DateTime Created { get; set; }
         public DateTime? Modified { get; set; }
         public string Folder { get; set; }
+
+        public VirusStatusValues VirusStatus { get; set; }
     }
 }
